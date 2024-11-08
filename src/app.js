10eb4f2ccb2f8b5ee734526c099909e4901a8b7b
Akash1816 <ap7239727@gmail.com>
2024-11-08 17:34:24 +0530

    advance routing concepts

--- src/app.js
+++ src/app.js
@@ -8,6 +8,34 @@ app.get("/user" , (req, res)=>{
     res.send({firstname : "Akash" , lastname : "pathak"});
 });
 
+//this will use /abc , /ac
+app.get("/ab?c" , (req, res)=>{
+    res.send({firstname : "Akash" , lastname : "pathak"});
+});
+
+
+//this will use /abc , /abbbbbbc , multiple numbers of b
+app.get("/ab+c" , (req, res)=>{
+    res.send({firstname : "Akash" , lastname : "pathak"});
+});
+
+//this will use /abcd , ab and anything in between and then cd 
+app.get("/ab*cd" , (req, res)=>{
+    res.send({firstname : "Akash" , lastname : "pathak"});
+});
+
+
+
+//this will use /abcd , in this bc is optional we can combin things too in this 
+app.get("/a(bc)?d" , (req, res)=>{
+    res.send({firstname : "Akash" , lastname : "pathak"});
+});
+
+//this will use /a, and it will use anything which uses a like cab car
+app.get(/a/ , (req, res)=>{
+    res.send({firstname : "Akash" , lastname : "pathak"});
+});
+
 app.post("/user" , (req,res)=>{
     console.log("save the data to the database");
     res.send("Data  has been successfully to the dtatabse ");
