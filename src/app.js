6a329253ec3655732fc9555dceac40b918fefcaa
Akash1816 <ap7239727@gmail.com>
2024-11-08 17:24:38 +0530

    exploring get , post , delete

--- src/app.js
+++ src/app.js
@@ -2,8 +2,25 @@ const express= require("express");
 
 const app= express();
 
-app.use("/test",(req,res)=>{
-    res.send("Namaste from the server ");  // request handler (name of this  function)
+
+//this will only handle GET calls to /user 
+app.get("/user" , (req, res)=>{
+    res.send({firstname : "Akash" , lastname : "pathak"});
+});
+
+app.post("/user" , (req,res)=>{
+    console.log("save the data to the database");
+    res.send("Data  has been successfully to the dtatabse ");
+});
+
+app.delete("/user",(req,res)=>{
+    console.log("data deleted ");
+    res.send("Data deleted!");
+});
+
+//this  will match all  the HTTP  method API calls to  /test  
+app.use("/test" , (req,res)=>{
+    res.send("Hello from the server ");
 });
 
 app.listen(3000 , ()=>{
